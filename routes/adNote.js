d9a1c8c68d21e999eff73e829e52ea8420812ebe
Kalix2255 <andreas.cvejic2@gmail.com>
2016-11-03 15:09:26 +0100

    Start project

--- routes/adNote.js
+++ routes/adNote.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET adNote listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('newNote');
 });
 
 module.exports = router;
